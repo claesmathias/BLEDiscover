85864c5f355094d045b06b4c1c2965383695086e
claesmathias <claesmathias@gmail.com>
2013-05-24 19:55:29 +0200

    add navigation drawer

--- BLEDiscover/src/main/java/com/claesinc/blediscover/MainActivity.java
+++ BLEDiscover/src/main/java/com/claesinc/blediscover/MainActivity.java
@@ -1,23 +1,60 @@
+/*
+ * Copyright (C) 2013 Mathias Claes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.claesinc.blediscover;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
 
+/**
+ * MainActivity class
+ *
+ * @author claesmathias@gmail.com (Mathias Claes)
+ */
 public class MainActivity extends Activity {
+    private String[] mMenuItems;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMenuItems = getResources().getStringArray(R.array.drawer_items);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mMenuItems));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
     }
 
+    private class DrawerItemClickListener implements AdapterView.OnItemClickListener {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+        }
     }
-    
 }
+
